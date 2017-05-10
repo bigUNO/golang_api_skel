a1126c1ded8a4f4d1e27d244735dc44154be8395
Leon Johnson <leon.johnson@gmail.com>
2017-05-10 15:51:27 -0400

    [MOD] linting

--- src/main_test.go
+++ src/main_test.go
@@ -12,6 +12,7 @@ func setup() {
 	id := xid.ID{0x4d, 0x88, 0xe1, 0x5b, 0x60, 0xf4, 0x86, 0xe4, 0x28, 0x41, 0x2d, 0xc9}
 	exampleTime := time.Date(2009, time.November, 10, 23, 0, 0, 0, time.UTC)
 	waldo := Item{Id: id, Name: "ghostfart", Modified: exampleTime}
+	fmt.Printf("%v", waldo)
 }
 
 func TestMain(m *testing.M) {
